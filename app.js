f5e8842e380cd4417d4edaef09997e1896c122db
Hypercoder0701 <IslomjonIsmoilov0701@gmail.com>
2023-12-25 13:32:46 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const paragraphs = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, sed corporis amet recusandae natus quia explicabo ullam atque? Necessitatibus, laboriosam?",
+];
+
+const typingText = document.querySelector(".typing-text p"),
+  inpField = document.querySelector(".wrapper .input-field"),
+  tryAgainBtn = document.querySelector(".content button"),
+  timeTag = document.querySelector(".time span b"),
+  mistakeTag = document.querySelector(".mistake span");
+
+let timer,
+  maxTime = 60,
+  timeLeft = maxTime,
+  charIndex = (mistakes = isTyping = 0);
+
+function loadParagraph() {
+  const ranIndex = Math.floor(Math.random() * paragraphs.length);
+  typingText.innerHTML = "";
+  paragraphs[ranIndex].split("").forEach((char) => {
+    let span = `<span>${char}</span>`;
+    typingText.innerHTML += span;
+  });
+  typingText.querySelectorAll("span")[0].classList.add("active");
+  document.addEventListener("keydown", () => inpField.focus());
+  typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+  let characters = typingText.querySelectorAll("span");
+  let typedChar = inpField.value.split("")[charIndex];
+  if (charIndex < characters.length - 1 && timeLeft > 0) {
+    if (!isTyping) {
+      timer = setInterval(initTimer, 1000);
+      isTyping = true;
+    }
+    if (typedChar == null) {
+      if (charIndex > 0) {
+        charIndex--;
+        if (characters[charIndex].classList.contains("incorrect")) {
+          mistakes--;
+        }
+        characters[charIndex].classList.remove("correct", "incorrect");
+      }
+    } else {
+      if (characters[charIndex].innerText == typedChar) {
+        characters[charIndex].classList.add("correct");
+      } else {
+        mistakes++;
+        characters[charIndex].classList.add("incorrect");
+      }
+      charIndex++;
+    }
+    characters.forEach((span) => span.classList.remove("active"));
+    characters[charIndex].classList.add("active");
+
+    mistakeTag.innerText = mistakes;
+  } else {
+    clearInterval(timer);
+    inpField.value = "";
+  }
+}
+
+function initTimer() {
+  if (timeLeft > 0) {
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+  } else {
+    clearInterval(timer);
+  }
+}
+
+function resetGame() {
+  loadParagraph();
+  clearInterval(timer);
+  timeLeft = maxTime;
+  charIndex = mistakes = isTyping = 0;
+  inpField.value = "";
+  timeTag.innerText = timeLeft;
+  mistakeTag.innerText = 0;
+}
+
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
